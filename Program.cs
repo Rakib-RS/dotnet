c9b330ecf31598fd9ca18fc434f3eb1176c732c5
Rakib Sheikh <rakib.rscse@gmail.com>
2018-05-01 19:15:09 +0600

    main function

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿#include "sum.cs"
+#include "mul.cs"
+using System;
+
+namespace c_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sum();
+            mul();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
